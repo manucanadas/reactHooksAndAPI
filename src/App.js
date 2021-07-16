ed0ef204236d18ab59dbaa706eaa53de87d9aade
Manuel Canadas <nanocanadas@gmail.com>
2021-07-16 16:11:47 +1200

    Fetch person's name from data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import './App.css';
+import './App.css'; 
+
+type Person = any;
 
 const fetchData = () => {
   return axios.get('https://randomuser.me/api/?results=20')
   .then((res) => {
-    console.log(res);
+    const {results} = res.data;
+    console.log(results);
+    return results;
   })
   .catch((err) => {
     console.error(err);
@@ -13,14 +17,21 @@ const fetchData = () => {
 }
 
 function App() {
+  const [people, setPeople] = useState([]);
+
   useEffect(() => {
-    fetchData();
+    fetchData().then(apiPeople => {
+      setPeople(apiPeople);
+    });
   }, [])
 
   return (
     <div className="App">
       <h1>Hello Manu!!</h1>
       <h2>Start editing to see some magic happen</h2>
+      {people.map((person: Person, personIdx) => <div key={personIdx}>
+        {person.name.first}
+      </div>)}
     </div>
   );
 }
